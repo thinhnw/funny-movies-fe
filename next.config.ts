853cb91d903b5bce398a87fde49adec4c0843853
thinhnw <thinhnv0412@gmail.com>
2025-02-18 17:47:39 +0700

    Configure Cloudflare deployment settings

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from "next";
+import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -12,5 +13,7 @@ const nextConfig: NextConfig = {
     ];
   },
 };
-
+if (process.env.NODE_ENV === "development") {
+  await setupDevPlatform();
+}
 export default nextConfig;
